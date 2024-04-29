57342141bbdaf4731216c01d8d3165454b089f32
felipewolmer <96786758+felipewolmer@users.noreply.github.com>
2024-04-29 18:43:52 -0300

    Update preloader.js

--- preloader.js
+++ preloader.js
@@ -1,36 +1,21 @@
-const overlay = document.createElement('div');
-overlay.id = 'loader';
-overlay.style.cssText = `
-    display: flex;
-    justify-content: center;
-    align-items: center;
+const lottieContainer = document.createElement('div');
+lottieContainer.id = 'lottieContainer';
+lottieContainer.style.cssText = `
     position: fixed;
     top: 0;
     left: 0;
     width: 100%;
     height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
     background-color: rgba(255, 255, 255, 0.7);
     z-index: 999;
 `;
 
-const lottieContainer = document.createElement('div');
-lottieContainer.id = 'lottieContainer';
-lottieContainer.style.cssText = `
-    max-width: 100%;
-    max-height: 100%;
-    display: none;
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-`;
-lottieContainer.style.display = 'none';
-
-document.body.appendChild(overlay);
 document.body.appendChild(lottieContainer);
 
 function hideOverlay() {
-    overlay.style.display = 'none';
     lottieContainer.style.display = 'block';
     // Substitua pela URL direta do seu arquivo JSON do Lottie.
     lottie.loadAnimation({
